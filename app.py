ced3073f8907ad30690ff90e4cf8640415d0de55
faby <fabianyevellyn9@gmail.com>
2026-07-17 10:12:52 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,55 +1,61 @@
 from flask import Flask, render_template, request, redirect, url_for
-from werkzeug.security import generate_password_hash, check_password_hash
+import json
+import os 
 
 app = Flask(__name__)
 
-# Variáveis globais (somente para fins didáticos)
-usuario_cadastrado = ""
-senha_hash = ""
+ARQUIVO = "filmes.json"
+
+def carregar_filmes():
+    """Lê o arquivo JSON e retorna uma lista de filmes."""
+
+    if not os.path.exists(ARQUIVO):
+       return []
+    
+    with open(ARQUIVO, "r", encoding="utf-8") as arquivo:
+        try:
+            return json.load(arquivo)
+        except json.JSONDecodeError:
+            return []
+        
+
+def salvar_filmes(lista_filmes):
+    """Salva a lista de filmes no arquivo JSON."""
+
+    with open(ARQUIVO, "w", ecoding="utf-8") as arquivo:
+        json.dump(
+            lista_filmes,
+            arquivo,
+            indent=4,
+            ensure_ascii=False
+        )
 
 
 @app.route("/", methods=["GET", "POST"])
 def cadastro():
-    global usuario_cadastrado, senha_hash
 
     if request.method == "POST":
-        usuario_cadastrado = request.form["nome"]
-        senha = request.form["senha"]
 
-        # Gera o hash da senha
-        senha_hash = generate_password_hash(senha)
+        filme = {
+            "nome": request.form["nome"],
+            "genero": request.form["genero"],
+            "ano": request.form["ano"]
+        }
 
-        return redirect(url_for("login"))
+        filmes = carregar_filmes()
+        filmes.append(filme)
+        salvar_filmes(filmes)
 
+        return redirect(url_for("listar"))
+    
     return render_template("cadastro.html")
 
+@app.route("/filmmes")
+def listar():
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    global usuario_cadastrado, senha_hash
+    filmes = carregar_filmes()
 
-    mensagem = ""
-
-    if request.method == "POST":
-        nome = request.form["nome"]
-        senha = request.form["senha"]
-
-        if nome != usuario_cadastrado:
-            mensagem = "Usuário não encontrado."
-
-        elif not check_password_hash(senha_hash, senha):
-            mensagem = "Senha inválida."
-
-        else:
-            return redirect(url_for("inicio"))
-
-    return render_template("login.html", mensagem=mensagem)
-
-
-@app.route("/inicio")
-def inicio():
-    return render_template("inicio.html", nome=usuario_cadastrado)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template(
+        "filmes.html",
+        filmes=filmes
+    )
